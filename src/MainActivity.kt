42c1457d6dbaa7f3353e8f5478f6e38eb0f61ab9
Armin <92685045+ArminRmt@users.noreply.github.com>
2022-03-16 14:58:23 +0330

    Create MainActivity.kt

---
+++ src/MainActivity.kt
@@ -0,0 +1,39 @@
+package ap.example.app
+
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import ap.example.app.ui.theme.AppTheme
+
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AppTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    ProfileApp()
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
